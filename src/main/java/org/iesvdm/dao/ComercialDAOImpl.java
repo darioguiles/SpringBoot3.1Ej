50cdd9b667ef8bf89974a2cccd246df567892a28
darioguiles <dario.guilescuesta@hotmail.com>
2023-12-20 08:53:31 +0100

    ComercialDAO Completado

--- src/main/java/org/iesvdm/dao/ComercialDAOImpl.java
+++ src/main/java/org/iesvdm/dao/ComercialDAOImpl.java
@@ -1,5 +1,6 @@
 package org.iesvdm.dao;
 
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
@@ -7,6 +8,8 @@ import org.iesvdm.modelo.Cliente;
 import org.iesvdm.modelo.Comercial;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import lombok.AllArgsConstructor;
@@ -21,11 +24,32 @@ public class ComercialDAOImpl implements ComercialDAO {
 
 	//JdbcTemplate se inyecta por el constructor de la clase automáticamente
 	//
+	@Autowired //Le ponemos autowired para que Lombok nos ayude con la creación del jdbc
 	private JdbcTemplate jdbcTemplate;
 	
 	@Override
 	public void create(Comercial cliente) {
-		// TODO Auto-generated method stub
+
+		String sqlInsert = """
+							INSERT INTO comercial (nombre, apellido1, apellido2, comisión) 
+							VALUES  (     ?,         ?,         ?,         ?)
+						   """;
+
+		KeyHolder keyHolder = new GeneratedKeyHolder();
+		//Con recuperación de id generado
+		int rows = jdbcTemplate.update(connection -> {
+			PreparedStatement ps = connection.prepareStatement(sqlInsert, new String[] { "id" });
+			int idx = 1;
+			ps.setString(idx++, cliente.getNombre());
+			ps.setString(idx++, cliente.getApellido1());
+			ps.setString(idx++, cliente.getApellido2());
+			ps.setFloat(idx, cliente.getComision());
+			return ps;
+		},keyHolder);
+
+		cliente.setId(keyHolder.getKey().intValue());
+
+		log.info("Insertados {} registros.", rows);
 
 	}
 
@@ -50,18 +74,53 @@ public class ComercialDAOImpl implements ComercialDAO {
 	@Override
 	public Optional<Comercial> find(int id) {
 		// TODO Auto-generated method stub
-		return Optional.empty();
+
+		Comercial fab =  jdbcTemplate
+				.queryForObject("SELECT * FROM comercial WHERE id = ?"
+						, (rs, rowNum) -> new Comercial(rs.getInt("id"),
+								rs.getString("nombre"),
+								rs.getString("apellido1"),
+								rs.getString("apellido2"),
+								rs.getFloat("comisión"))
+						, id
+				);
+
+		if (fab != null) {
+			return Optional.of(fab);}
+		else{
+			log.info("Comercial no encontrado.");
+			return Optional.empty();
+		}
+
 	}
 
 	@Override
 	public void update(Comercial cliente) {
-		// TODO Auto-generated method stub
+
+		int rows = jdbcTemplate.update("""
+										UPDATE comercial SET 
+														nombre = ?, 
+														apellido1 = ?, 
+														apellido2 = ?,
+														comisión = ?  
+												WHERE id = ?
+										""", cliente.getNombre()
+				, cliente.getApellido1()
+				, cliente.getApellido2()
+				, cliente.getComision()
+				, cliente.getId());
+
+		log.info("Update de Comercial con {} registros actualizados.", rows);
+
 
 	}
 
 	@Override
 	public void delete(long id) {
-		// TODO Auto-generated method stub
+
+		int rows = jdbcTemplate.update("DELETE FROM comercial WHERE id = ?", id);
+
+		log.info("Delete de Comercial con {} registros eliminados.", rows);
 
 	}
 

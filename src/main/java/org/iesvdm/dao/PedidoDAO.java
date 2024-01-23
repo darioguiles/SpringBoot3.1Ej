956d845823f8ce34fdde29e7eb3b3cafb7f7afde
darioguiles <dario.guilescuesta@hotmail.com>
2024-01-23 21:47:43 +0100

    Falta HTML Pedido, Y reincorporar la Lista de Pedido a Cliente/Comercial  Lógica DTO

--- src/main/java/org/iesvdm/dao/PedidoDAO.java
+++ src/main/java/org/iesvdm/dao/PedidoDAO.java
@@ -1,25 +1,30 @@
 package org.iesvdm.dao;
 
 
+import org.iesvdm.modelo.Cliente;
+import org.iesvdm.modelo.Comercial;
 import org.iesvdm.modelo.Pedido;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface PedidoDAO {
+public interface PedidoDAO<T extends Pedido> extends RepositoryBase<T> {
 
-    public void create(Pedido pedido);
-
-    public List<Pedido> getAll();
+    /*
 
     public List<Pedido> getAllIDComercial(int id);
 
     public List<Pedido> getAllIDCliente(int id);
+*/
 
-    public Optional<Pedido> find(int id);
+    public Optional<Cliente> findClienteBy(int pedidoId);
 
-    public void update(Pedido pedido);
+    public Optional<Comercial> findComercialBy(int pedidoId);
 
-    public void delete(long id);
+
+    public List<Cliente> getAllClientesByIdPedido(int pedidoId);
+
+    public void updateSinComercial(T t);
+
 
 }
